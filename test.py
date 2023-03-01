623682f2651c4790a6de590c6087b7acc068769f
donghaozhang <zdhpeter1991@gmail.com>
2023-03-01 23:29:45 +1100

    clean up the prints

--- test.py
+++ test.py
@@ -105,6 +105,96 @@ def plot_confusion_matrix(confusion_matrix, classes, normalize=False, title='Con
     plt.show()
 
 
+def compute_exact_calibration_error(pred, gt, num_classes):
+    """
+    Compute the exact calibration error.
+    :param pred: predicted logits
+    :param gt: ground truth labels
+    :param num_classes: number of classes
+    :return: exact calibration error
+    """
+    confusion_matrix = compute_confusion_matrix(pred, gt, num_classes)
+    row_sum = np.sum(confusion_matrix, axis=1)
+    col_sum = np.sum(confusion_matrix, axis=0)
+    accuracy = np.sum(np.diag(confusion_matrix)) / np.sum(confusion_matrix)
+    ece = 0
+    for i in range(num_classes):
+        ece += np.abs(row_sum[i] / np.sum(row_sum) - col_sum[i] / np.sum(col_sum))
+    ece = ece / 2
+    return ece, accuracy
+
+
+def calc_bins(pred_probs, labels_oneh):
+    # Assign each prediction to a bin
+    num_bins = 10
+    bins = np.linspace(0.1, 1, num_bins)
+    binned = np.digitize(pred_probs, bins)
+
+    # Save the accuracy, confidence and size of each bin
+    bin_accs = np.zeros(num_bins)
+    bin_confs = np.zeros(num_bins)
+    bin_sizes = np.zeros(num_bins)
+
+    for bin in range(num_bins):
+        bin_sizes[bin] = len(pred_probs[binned == bin])
+    if bin_sizes[bin] > 0:
+      bin_accs[bin] = (labels_oneh[binned==bin]).sum() / bin_sizes[bin]
+      bin_confs[bin] = (pred_probs[binned==bin]).sum() / bin_sizes[bin]
+
+    return bins, binned, bin_accs, bin_confs, bin_sizes
+
+
+def get_metrics(pred_probs, labels_oneh):
+    ECE = 0
+    MCE = 0
+    bins, _, bin_accs, bin_confs, bin_sizes = calc_bins(pred_probs, labels_oneh)
+
+    for i in range(len(bins)):
+        abs_conf_dif = abs(bin_accs[i] - bin_confs[i])
+        ECE += (bin_sizes[i] / sum(bin_sizes)) * abs_conf_dif
+        MCE = max(MCE, abs_conf_dif)
+
+    return ECE, MCE
+
+
+def draw_reliability_graph(pred_probs, labels_oneh):
+    ECE, MCE = get_metrics(pred_probs, labels_oneh)
+    bins, _, bin_accs, _, _ = calc_bins(pred_probs, labels_oneh)
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.gca()
+
+    # x/y limits
+    ax.set_xlim(0, 1.05)
+    ax.set_ylim(0, 1)
+
+    # x/y labels
+    plt.xlabel('Confidence')
+    plt.ylabel('Accuracy')
+
+    # Create grid
+    ax.set_axisbelow(True)
+    ax.grid(color='gray', linestyle='dashed')
+
+    # Error bars
+    plt.bar(bins, bins, width=0.1, alpha=0.3, edgecolor='black', color='r', hatch='\\')
+
+    # Draw bars and identity line
+    plt.bar(bins, bin_accs, width=0.1, alpha=1, edgecolor='black', color='b')
+    plt.plot([0, 1], [0, 1], '--', color='gray', linewidth=2)
+
+    # Equally spaced axes
+    plt.gca().set_aspect('equal', adjustable='box')
+
+    # ECE and MCE legend
+    ECE_patch = mpatches.Patch(color='green', label='ECE = {:.2f}%'.format(ECE * 100))
+    MCE_patch = mpatches.Patch(color='red', label='MCE = {:.2f}%'.format(MCE * 100))
+    plt.legend(handles=[ECE_patch, MCE_patch])
+
+    plt.savefig('results/calibrated_graph.png', bbox_inches='tight')
+    plt.show()
+
+
 def main(opt):
     runner = build_runner(opt)
     if opt.checkpoint is not None:
@@ -116,6 +206,10 @@ def main(opt):
     print('---Compute and plot the confusion matrix---')
     cm2 = compute_confusion_matrix(pred, gt, 10)
     plot_confusion_matrix(cm2, classes=cifar10, normalize=False)
+    print('---Compute the Expected Calibration Error (ECE) and Max Calibration Error (MCE)---')
+    ECE, MCE = get_metrics(pred_probs, labels_oneh)
+    print('Exact calibration error: {:.2f}%'.format(ECE * 100), 'Max Calibration Error: {:.2f}%'.format(MCE * 100))
+    draw_reliability_graph(pred_probs, labels_oneh)
 
 
 if __name__ == '__main__':
